92e85d5f8aed12d0bdc5fd9f40a01f7c25f10b5d
pjvds <pj@born2code.net>
2011-03-03 02:11:30 -0800

    Fixed count down logic. It now counts down correctly.

--- js/pomodo.js
+++ js/pomodo.js
@@ -4,53 +4,56 @@
 	p.start();
 	
 	document.observe('pomodorotimer:tick', function(evt) {
-		$('t').innerHTML = evt.memo.timeText;
-	});
+      $('t').innerHTML = evt.memo.timeText;
+    });
   }
 
   var PomodoroTimer = Class.create({
-  
-	initialize: function() {
-		this.total = 0.0;
-	},
-  
-	start: function(milliseconds) {
-		this.millisecondsLeft = milliseconds;
-		this.base = this.getTime();
-		this.lastUpdate = this.base;
-		
-		document.fire('pomodorotimer:started');
-		this.update();
-	},
-	
-	update: function() {
-		this.secondsLeft;
-		
-		var diffInMs = this.getTime()-this.lastUpdate;
-		this.millisecondsLeft -= diffInMs;
-		
-		// Update time and fire tick event.
-		this.lastUpdate = this.getTime();
-		this.fireTickEvent();
-		
-		// Schedule next update.
-		dt = 1000 - (this.getTime()-this.base)%1000;
-		this.timer = setTimeout(function(thisObj) {thisObj.update();}, dt, this);
-	},
-	
-	fireTickEvent: function() {
-		var t = this.millisecondsLeft;
-		var s = Math.round(t/1000);
-		var m = Math.floor(s/60);
-		var h = Math.floor(m/60);
-		s=s%60;
-	
-		document.fire('pomodorotimer:tick', {milliseconds: this.millisecondsLeft, 
-											 seconds: s, minutes: m, hours: h});
-	},
-	
-	getTime: function() {	
-		var now = new Date;
-		return now.getTime();
-	}
+    
+    initialize: function() {
+      this.total = 0.0;
+    },
+    
+    start: function(milliseconds) {
+      this.millisecondsLeft = milliseconds;
+      this.base = this.getTime();
+      this.lastUpdate = this.base;
+      
+      document.fire('pomodorotimer:started');
+      this.update();
+    },
+    
+    update: function() {
+      this.secondsLeft;
+      
+      var diffInMs = this.getTime()-this.lastUpdate;
+      this.millisecondsLeft -= diffInMs;
+      
+      // Update time and fire tick event.
+      this.lastUpdate = this.getTime();
+      this.fireTickEvent();
+      
+      // Schedule next update.
+      dt = 1000 - (this.getTime()-this.base)%1000;
+      this.timer = setTimeout(function(thisObj) {thisObj.update();}, dt, this);
+    },
+    
+    fireTickEvent: function() {
+      var t = this.millisecondsLeft;
+      var hours = Math.floor(t/60/60/1000);
+      t = t-(hours*60*60*1000);
+      
+      var minutes = Math.floor(t/60/1000);
+      t = t-(minutes*60*1000);
+      
+      var seconds = Math.round(t/1000);
+    
+      document.fire('pomodorotimer:tick', {milliseconds: this.millisecondsLeft, 
+                         seconds: seconds, minutes: minutes, hours: hours});
+    },
+    
+    getTime: function() {	
+      var now = new Date;
+      return now.getTime();
+    }
   });
