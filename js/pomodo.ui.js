9d7c1e84e441694d57871ac2c05484dd75938600
pjvds <pj@born2code.net>
2011-03-08 00:14:47 +0100

    Moved Google Analytics to Pomodo.ui.js.

--- js/pomodo.ui.js
+++ js/pomodo.ui.js
@@ -82,6 +82,16 @@ function wireUserInterfaceCommandEvents() {
 	})
 }
 function init() {
+	var _gaq = _gaq || [];
+	_gaq.push(['_setAccount', 'UA-21890950-1']);
+	_gaq.push(['_trackPageview']);
+
+	(function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	})();
+	
 	jQuery.noConflict();
 	wirePomodoroEvents();
 	wireUserInterfaceCommandEvents();
